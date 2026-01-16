7d0c628c3ad81fe16dd96e00a5623a1f3fd67f10
RSAlbuquerque <105561840+RSAlbuquerque@users.noreply.github.com>
2026-01-16 16:48:57 +0000

    Delete compdb.py

--- compdb.py
+++
@@ -1,9 +0,0 @@
-import os
-
-Import("env")  # type: ignore
-
-# include toolchain paths
-env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)  # type: ignore
-
-# override compilation DB path
-env.Replace(COMPILATIONDB_PATH=os.path.join("$BUILD_DIR", "compile_commands.json"))  # type: ignore
